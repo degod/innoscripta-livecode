29654db90a116b682a2d66e5701eea982e539b66
degod <degodtest@gmail.com>
2025-11-10 18:13:55 +0100

    CRUD for Transaction API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\Transaction\CreateController;
+use App\Http\Controllers\Transaction\DeleteController;
 use App\Http\Controllers\Transaction\FetchController;
+use App\Http\Controllers\Transaction\UpdateController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/transactions', FetchController::class)->name('transactions.fetch');
+Route::post('/transaction/create', CreateController::class)->name('transactions.create');
+Route::put('/transaction/update', UpdateController::class)->name('transactions.update');
+Route::delete('/transaction/delete', DeleteController::class)->name('transactions.delete');
