a8d1b976e7f956bdd631a63f2af89e941c64fbe1
degod <degodtest@gmail.com>
2025-11-10 18:18:17 +0100

    Handle 4xx and 5xx errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Exceptions\InvalidTransactionException;
+use App\Services\ResponseService;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,5 +25,27 @@ return Application::configure(basePath: dirname(__DIR__))
             Log::channel('financial_emergency')->error($e->getMessage(), [
                 'id' => $e->recordId,
             ]);
+        });
+
+
+        $exceptions->render(function (Throwable $e, Request $request) {
+            if (! $request->is('api/*')) {
+                return null;
+            }
+
+            $responseService = app(ResponseService::class);
+
+            // Handle 4xx and 5xx HTTP exceptions
+            if ($e instanceof HttpExceptionInterface) {
+                $status = $e->getStatusCode();
+                $message = $e->getMessage() ?: Response::$statusTexts[$status] ?? 'HTTP Error';
+
+                return $responseService->error($status, $message);
+            }
+
+            return $responseService->error(
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+                'Something went wrong on the server.'
+            );
         });
     })->create();
